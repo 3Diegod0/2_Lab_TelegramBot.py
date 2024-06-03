99970c5bbb26ddc8f4d86dc3a40c3434c9b0b9d1
3Diegod0 <gubkabobizspan4boba@list.ru>
2024-06-03 14:02:51 +0600

    Update TelegramBot.py

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,41 +1,73 @@
 import logging
-import random
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
-import asyncio
-import os
-from dotenv import load_dotenv
-
+from aiogram import Bot, Dispatcher, executor, types
 
 logging.basicConfig(level=logging.INFO)
+TOKEN = "7095651321:AAGMwvocGc3g_tc3vxHtVQ3bKYeovDsgkIA"
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
-load_dotenv()
+# Начальное состояние игры
+game_state = {
+    "number": None,
+    "attempts": 0,
+    "min_value": 1,
+    "max_value": 100
+}
 
-bot = Bot(os.getenv('TOKEN'))
-dp = Dispatcher()
-
-random_number = random.randint(1, 100)
-attempts = 0
-
-@dp.message(Command('start'))
-async def start(message: types.Message):
-    global random_number, attempts
-    random_number = random.randint(1, 100)
-    attempts = 0
-    await message.answer("Привет! Я загадал число от 1 до 100. Попробуй угадать!")
-
-@dp.message()
-async def guess_number(message: types.Message):
-    global random_number, attempts
-    user_number = int(message.text)
-    attempts += 1
-
-    if user_number == random_number:
-        await message.answer(f"Поздравляю, вы угадали число за {attempts} попыток!")
-    elif user_number < random_number:
-        await message.answer("Загаданное число больше. Попробуйте еще раз!")
+# Обработчик команды /start
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    if len(message.text.split()) > 1:
+        try:
+            number = int(message.text.split()[1])
+            if number < game_state["min_value"] or number > game_state["max_value"]:
+                await message.reply(f"Пожалуйста, загадай число в диапазоне от {game_state['min_value']} до {game_state['max_value']}.")
+                return
+            game_state["number"] = number
+            await message.reply("Отлично! Я начинаю угадывать.")
+            await ask_question(message)
+        except ValueError:
+            await message.reply("Пожалуйста, загадай целое число после команды /start.")
     else:
-        await message.answer("Загаданное число меньше. Попробуйте еще раз!")
+        await message.reply("Пожалуйста, загадай число после команды /start, например: /start 42")
+
+# Обработчик ответов на вопросы
+@dp.message_handler()
+async def handle_answer(message: types.Message):
+    if game_state["number"] is None:
+        return
+
+    answer = message.text.lower()
+
+    if answer == "больше":
+        if game_state["guess"] < game_state["number"]:
+            game_state["min_value"] = game_state["guess"] + 1
+        else:
+            await message.reply("Не пытайся меня обмануть! Я уже угадал число.")
+            return
+    elif answer == "меньше":
+        if game_state["guess"] > game_state["number"]:
+            game_state["max_value"] = game_state["guess"] - 1
+        else:
+            await message.reply("Не пытайся меня обмануть! Я уже угадал число.")
+            return
+    elif answer == "угадал":
+        await message.reply(f"Ура! Я угадал число {game_state['guess']} за {game_state['attempts']} попыток.")
+        game_state["number"] = None
+        game_state["attempts"] = 0
+        game_state["min_value"] = 1
+        game_state["max_value"] = 100
+        return
+    else:
+        await message.reply("Пожалуйста, ответьте 'больше', 'меньше' или 'угадал'.")
+        return
+
+    game_state["attempts"] += 1
+    await ask_question(message)
+
+async def ask_question(message: types.Message):
+    game_state["guess"] = (game_state["min_value"] + game_state["max_value"]) // 2
+    await message.reply(f"Мое предположение: {game_state['guess']}. Загаданное число больше, меньше или я угадал?")
 
 async def main():
     await  bot.delete_webhook(drop_pending_updates=True)
